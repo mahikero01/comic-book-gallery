698bce405a0ef366efd83f97d58ef6cf4f6d6781
mahikero <federicoparassarmiento@yahoo.com>
2016-07-13 08:11:08 -0400

    up

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,18 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-               
-            }
-
-
-
-
-            return Content("this is comic");
-            
-            
+            return View();
             
         }
     }
